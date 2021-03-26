262df6f1da2f7fc2f08dd49646ed650374ca1128
Maximiliano Nicolás Güelpa <mguelpa@gmail.com>
2021-03-26 00:36:59 -0300

    added review of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { routesSlideinAnimation, sideNavAnimation } from './app.animation';
+import { routesSlideinAnimation, sideNavAnimation } from './modules/app.animation';
 import { LoadingEventService } from './services/loading-event.service';
 
 @Component({
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,25 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopNavComponent } from './components/shared/top-nav/top-nav.component';
+import { AppComponent } from '../app.component';
+import { TopNavComponent } from '../components/shared/top-nav/top-nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from './../environments/environment';
-import { HomeComponent } from './routes/home/home.component';
-import { NotFoundComponent } from './routes/not-found/not-found.component';
-import { DeniedComponent } from './routes/denied/denied.component';
-import { SigninComponent } from './routes/signin/signin.component';
-import { SigninFormComponent } from './components/signin/signin-form/signin-form.component';
+import { environment } from '../../environments/environment';
+import { HomeComponent } from '../routes/home/home.component';
+import { NotFoundComponent } from '../routes/not-found/not-found.component';
+import { DeniedComponent } from '../routes/denied/denied.component';
+import { SigninComponent } from '../routes/signin/signin.component';
+import { SigninFormComponent } from '../components/signin/signin-form/signin-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SpinnerComponent } from './components/shared/spinner/spinner.component';
-import { SideNavComponent } from './components/shared/side-nav/side-nav.component';
+import { SpinnerComponent } from '../components/shared/spinner/spinner.component';
+import { SideNavComponent } from '../components/shared/side-nav/side-nav.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AboutMeComponent } from './routes/about-me/about-me.component';
+import { AboutMeComponent } from '../routes/about-me/about-me.component';
+import { AboutProjectComponent } from '../routes/about-project/about-project.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AboutMeComponent } from './routes/about-me/about-me.component';
     SigninFormComponent,
     SpinnerComponent,
     SideNavComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    AboutProjectComponent
   ],
   imports: [
     BrowserModule,
